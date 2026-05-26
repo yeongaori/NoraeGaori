3de33450d33d31e1ff6f8cc085618942a5292bd0
yeongaori <yeongor@gmail.com>
2026-05-26 18:21:28 +0900

    Fix regex for forceremove

--- internal/commands/forceremove.go
+++ internal/commands/forceremove.go
@@ -3,11 +3,13 @@ package commands
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 
 	"github.com/bwmarrin/discordgo"
 	"noraegaori/internal/messages"
 	"noraegaori/internal/player"
 	"noraegaori/internal/queue"
+	"noraegaori/pkg/logger"
 )
 
 func HandleForceRemove(s *discordgo.Session, i *discordgo.InteractionCreate) error {
@@ -19,21 +21,35 @@ func HandleForceRemove(s *discordgo.Session, i *discordgo.InteractionCreate) err
 	target := options[0].StringValue()
 
 	q, err := queue.GetQueue(i.GuildID, false)
-	if err != nil || q == nil || len(q.Songs) < 2 {
+	if err != nil || q == nil || len(q.Songs) == 0 {
 		RespondEmbed(s, i, messages.CreateErrorEmbed(messages.T(i.GuildID).Titles.Error, messages.T(i.GuildID).Admin.NoSongsToDelete))
 		return nil
 	}
 
+	if regexp.MustCompile(`^\d+$`).MatchString(target) {
+		if len(target) >= 17 {
+			return forceRemoveByUser(s, i, q, target)
+		}
+		position, _ := strconv.Atoi(target)
+		return forceRemoveByPosition(s, i, q, position)
+	}
+
 	userMentionRegex := regexp.MustCompile(`^<@!?(\d+)>$`)
 	matches := userMentionRegex.FindStringSubmatch(target)
-
 	if len(matches) < 2 {
 		RespondEmbed(s, i, messages.CreateErrorEmbed(messages.T(i.GuildID).Titles.Error,
 			messages.T(i.GuildID).Admin.InvalidMention))
 		return nil
 	}
 
-	targetUserID := matches[1]
+	return forceRemoveByUser(s, i, q, matches[1])
+}
+
+func forceRemoveByUser(s *discordgo.Session, i *discordgo.InteractionCreate, q *queue.Queue, targetUserID string) error {
+	if len(q.Songs) < 2 {
+		RespondEmbed(s, i, messages.CreateErrorEmbed(messages.T(i.GuildID).Titles.Error, messages.T(i.GuildID).Admin.NoSongsToDelete))
+		return nil
+	}
 
 	targetUser, err := s.User(targetUserID)
 	if err != nil {
@@ -42,7 +58,7 @@ func HandleForceRemove(s *discordgo.Session, i *discordgo.InteractionCreate) err
 	}
 
 	var userSongs []*queue.Song
-	startIdx := 1 
+	startIdx := 1
 	if !q.Playing && !q.Loading {
 		startIdx = 0
 	}
@@ -84,7 +100,6 @@ func HandleForceRemove(s *discordgo.Session, i *discordgo.InteractionCreate) err
 		return err
 	}
 
-	
 	if isNextSongRemoved {
 		player.CleanupPreCacheWorker(i.GuildID)
 	}
@@ -92,5 +107,59 @@ func HandleForceRemove(s *discordgo.Session, i *discordgo.InteractionCreate) err
 	embed := messages.CreateSuccessEmbed(messages.T(i.GuildID).Admin.DeleteCompleteTitle,
 		fmt.Sprintf(messages.T(i.GuildID).Admin.DeleteCompleteDesc, messages.EscapeMarkdown(targetUser.Username), len(userSongs)))
 	RespondEmbed(s, i, embed)
+	return nil
+}
+
+func forceRemoveByPosition(s *discordgo.Session, i *discordgo.InteractionCreate, q *queue.Queue, position int) error {
+	if position < 1 || position > len(q.Songs) {
+		RespondEmbed(s, i, messages.CreateErrorEmbed(messages.T(i.GuildID).Titles.Error,
+			fmt.Sprintf(messages.T(i.GuildID).Admin.EnterValidRange, len(q.Songs))))
+		return nil
+	}
+
+	if position == 1 && (q.Playing || q.Loading) {
+		return forceRemoveCurrent(s, i, q)
+	}
+
+	songToRemove := q.Songs[position-1]
+	isNextSongRemoved := len(q.Songs) > 1 && q.Songs[1].ID == songToRemove.ID
+
+	if err := queue.RemoveSong(i.GuildID, position-1); err != nil {
+		RespondEmbed(s, i, messages.CreateErrorEmbed(messages.T(i.GuildID).Titles.Error,
+			fmt.Sprintf(messages.T(i.GuildID).Admin.RemoveFailed, err)))
+		return err
+	}
+
+	if isNextSongRemoved {
+		player.CleanupPreCacheWorker(i.GuildID)
+	}
+
+	embed := messages.CreateSuccessEmbed(messages.T(i.GuildID).Queue.SongsRemovedTitle,
+		fmt.Sprintf(messages.T(i.GuildID).Queue.SongRemoved, messages.EscapeMarkdown(songToRemove.Title)))
+	RespondEmbed(s, i, embed)
+	return nil
+}
+
+func forceRemoveCurrent(s *discordgo.Session, i *discordgo.InteractionCreate, q *queue.Queue) error {
+	DeferResponse(s, i)
+
+	songTitle := q.Songs[0].Title
+
+	err := player.Skip(s, i.GuildID)
+	if err != nil && err != player.ErrQueueEmpty {
+		UpdateResponseEmbed(s, i, messages.CreateErrorEmbed(messages.T(i.GuildID).Titles.Error,
+			fmt.Sprintf(messages.T(i.GuildID).Admin.RemoveFailed, err)))
+		return nil
+	}
+
+	if err == player.ErrQueueEmpty {
+		if stopErr := player.Stop(i.GuildID); stopErr != nil {
+			logger.Errorf("[ForceRemove] Failed to cleanup after queue empty: %v", stopErr)
+		}
+	}
+
+	embed := messages.CreateSuccessEmbed(messages.T(i.GuildID).Queue.SongsRemovedTitle,
+		fmt.Sprintf(messages.T(i.GuildID).Queue.SongRemoved, messages.EscapeMarkdown(songTitle)))
+	UpdateResponseEmbed(s, i, embed)
 	return nil
 }
